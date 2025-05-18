dc8058816eca5519d83c495550e829699b9edcfb
Tengyuu <springy10007@gmail.com>
2025-05-18 18:10:50 +0800

    a

--- Final-Project/Form3.cs
+++ Final-Project/Form3.cs
@@ -13,33 +13,39 @@ namespace Final_Project
 {
     public partial class Sliding_Puzzle: Form
     {
+        //全域
+        Image[] ans_imgs = new Image[9];
+        Bitmap img = null;
         public Sliding_Puzzle()
         {
             InitializeComponent();
         }
-        Image[] ans_imgs = new Image[9];
+        
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Title = "選擇圖片";
-            openFileDialog1.Filter = "圖片檔 (*.jpg; *.jpeg; *.png)|*.jpg;*.jpeg;*.png";
-            Image original;
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
             {
-                string imgpath = openFileDialog1.FileName;
-                using(var fs = new FileStream(imgpath,FileMode.Open,FileAccess.Read))
+                openFileDialog1.Title = "選擇圖片";
+                openFileDialog1.Filter = "圖片檔 (*.jpg; *.jpeg; *.png)|*.jpg;*.jpeg;*.png";
+                Image original;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    original = Image.FromStream(fs);
-                }
-                Bitmap img = new Bitmap(original, new Size(270, 270));    
-                original.Dispose();//釋放圖資源
-                pictureBox10.Image = img;
-                pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
-                for (int i = 0; i < 9; i++)
-                {
-                    ans_imgs[i] = img.Clone(new Rectangle(i % 3 * 90, i / 3 * 90, 90, 90), img.PixelFormat);
+                    string imgpath = openFileDialog1.FileName;
+                    using (var fs = new FileStream(imgpath, FileMode.Open, FileAccess.Read))
+                    {
+                        original = Image.FromStream(fs);
+                    }
+                    img = new Bitmap(original, new Size(270, 270));
+                    original.Dispose();//釋放圖資源
+                    pictureBox10.Image = img;
+                    pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
+                    for (int i = 0; i < 9; i++)
+                    {
+                        ans_imgs[i] = img.Clone(new Rectangle(i % 3 * 90, i / 3 * 90, 90, 90), img.PixelFormat);
+                    }
                 }
             }
+                
         }
 
         private void Sliding_Puzzle_Load(object sender, EventArgs e)
@@ -51,6 +57,16 @@ namespace Final_Project
                 //pb.Click += Puzzle_Pb_Click;
             }
             panel1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(img == null)
+            {
+                MessageBox.Show("請先選擇一張圖片!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
         }
     }
 }
